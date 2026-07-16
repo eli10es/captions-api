7674c9bcfbe1a5da406e1e746db8a9d80ae55be8
eli10es <eli10es@yahoo.es>
2026-07-16 23:40:51 +0300

    implement show method

--- captions-api/app/controllers/captions_controller.rb
+++ captions-api/app/controllers/captions_controller.rb
@@ -3,4 +3,17 @@ class CaptionsController < ApplicationController
     captions = Caption.all
     render json: { captions: captions }
   end
+
+  def show
+    caption = Caption.find_by(id: params[:id])
+    if caption
+      render json: caption
+    else
+      render json: {
+        code: "missing_parameters",
+        title: "Parameter is missing from the request body",
+        description: "url parameter is missing from the request body. It is a required parameter and the request cannot be processed without it"
+      }, status: :not_found
+    end
+  end
 end
--- captions-api/spec/requests/captions_spec.rb
+++ captions-api/spec/requests/captions_spec.rb
@@ -17,5 +17,17 @@ RSpec.describe "Captions", type: :request do
       json = JSON.parse(response.body)
       expect(json["captions"].length).to eq(1)
     end
+
+    it 'returns a specific caption' do
+      caption = Caption.create!(url: "https://example.com/random_image.jpg", text: "random_text", caption_url: "https://server/public/random_uuid.jpg")
+      get caption_path(id: caption.id)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns 404 for a non-existent caption' do
+      get caption_path(id: 67)
+      expect(response).to have_http_status(:not_found)
+    end
+
   end
 end
