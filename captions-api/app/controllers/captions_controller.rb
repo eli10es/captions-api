f2acddb7a03160cbf707b64dc7f8797a5ee3c8a5
eli10es <eli10es@yahoo.es>
2026-07-16 19:07:20 +0300

    add get endpoint

--- captions-api/app/controllers/captions_controller.rb
+++ captions-api/app/controllers/captions_controller.rb
@@ -1,2 +1,6 @@
 class CaptionsController < ApplicationController
+  def index
+    captions = Caption.all
+    render json: { captions: captions }
+  end
 end
--- captions-api/spec/requests/captions_spec.rb
+++ captions-api/spec/requests/captions_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Captions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /captions" do
+
+    it "returns http success" do
+      get captions_path
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+      expect(json["captions"]).to eq([])
+    end
+
+    it "returns captions" do
+      caption = Caption.create!(url: "https://example.com/random_image.jpg", text: "random_text", caption_url: "https://server/public/random_uuid.jpg")
+      get captions_path
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+      expect(json["captions"].length).to eq(1)
+    end
   end
 end
