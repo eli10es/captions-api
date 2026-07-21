06d4cdce1b74291ba5fee4d628811cdb6fbac81a
eli10es <eli10es@yahoo.es>
2026-07-21 21:31:01 +0300

    return correct json format

--- captions-api/app/controllers/captions_controller.rb
+++ captions-api/app/controllers/captions_controller.rb
@@ -6,12 +6,12 @@ class CaptionsController < ApplicationController
 
   def index
     captions = Caption.all
-    render json: { captions: captions }
+    render json: { captions: captions.map { |caption| caption_json(caption) } }
   end
 
   def show
-    caption = Caption.find(id: params[:id])
-    render json: caption
+    caption = Caption.find(params[:id])
+    render json: { caption: caption_json(caption) }
   end
 
   def create
@@ -23,16 +23,16 @@ class CaptionsController < ApplicationController
     result_url = Generator.new(Downloader.new, Captioner.new).generate(params[:caption][:text], params[:caption][:url])
     caption = Caption.new(url: params[:caption][:url], text: params[:caption][:text], caption_url: result_url)
     caption.save!
-      render json: caption, status: :created
+    render json: { caption: caption_json(caption) }, status: :created
   end
 
   def destroy
-    caption = Caption.find(id: params[:id])
-    binding.pry
-    file_path = "./tmp/images/#{caption.caption_url}.jpg"
+    caption = Caption.find(params[:id])
+    file_name = File.basename(caption.caption_url)
+    file_path = "./public/#{file_name}"
     File.delete(file_path) if File.exist?(file_path)
     caption.destroy!
-      render status: :ok
+    render status: :ok
   end
 
 
@@ -53,4 +53,9 @@ class CaptionsController < ApplicationController
   def render_unprocessable_entity(exception)
     render_error("invalid_attributes", "Unprocessable Entity", exception.message, :unprocessable_entity)
   end
+
+  def caption_json(caption)
+    caption.as_json(only: [:id, :url, :text, :caption_url])
+  end
+
 end
